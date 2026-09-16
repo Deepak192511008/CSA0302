c18eb854e51a162cf2e4837e118601845d890b9d
Deepak192511008 <192511008.simats@saveetha.com>
2026-09-16 19:11:05 +0530

    Create 2nd lar.c

---
+++ Lab/2nd lar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, large, second;
+
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++)
+        scanf("%d", &a[i]);
+
+    large = second = -99999;
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i] > large)
+        {
+            second = large;
+            large = a[i];
+        }
+        else if(a[i] > second && a[i] != large)
+            second = a[i];
+    }
+
+    printf("%d", second);
+
+    return 0;
+}
