21c0ca6a500c4f1268e82d728bc0ce84291179ce
Deepak192511008 <192511008.simats@saveetha.com>
2026-09-16 20:23:50 +0530

    Create sort alpha.c

---
+++ Lab/sort alpha.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100], temp;
+    int i, j, n;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    n = strlen(str);
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (str[j] > str[j + 1])
+            {
+                temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Alphabetical order: %s", str);
+
+    return 0;
+}
